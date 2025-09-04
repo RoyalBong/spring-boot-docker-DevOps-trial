6262803f25ae82178f1b3e7a2be4f90753923ece
Shayan Dutta <shayandutta97@gmail.com>
2025-09-05 04:10:13 +0530

    Updated Jenkins FIle

--- src/main/java/com/example/demo/SpringBootDockerApplication.java
+++ src/main/java/com/example/demo/SpringBootDockerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class SpringBootDockerApplication {
 	
-	@GetMapping("/message")
+	@GetMapping()
    public String getmessage() {
 	   return "Welcome to Docker!!!";
    }
